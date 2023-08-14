dcdf54a5472235a17d2e2742838f6e945a4d8e02
Aur Daniel <126253312+AurDaniel@users.noreply.github.com>
2023-08-15 00:03:20 +0300

    First commit

---
+++ ImageFeed/ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,15 @@
+import Foundation
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    
+    static let reuseIdentifier = "ImagesListCell"
+    
+    
+    @IBOutlet var imageCell: UIImageView!
+    
+    @IBOutlet var likeButton: UIButton!
+    
+    @IBOutlet var dateLabel: UILabel!
+    
+}
