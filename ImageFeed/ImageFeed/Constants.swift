2cb54b85130713d05179bf3b37b5e352ae49a0b6
Aur Daniel <aurdaniel@ya.ru>
2023-09-11 17:52:11 +0300

    Подготовка работы с Unsplash

---
+++ ImageFeed/ImageFeed/Constants.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+struct Constants {
+    
+    let AccessKey = "81KioUF0JhmIQnYWOW_mL5185yxGyVmkdWMjqdyRZVE"
+    let SecretKey = "zHghwVLlmB8_Im-1y8A3fr-FzMFaAxufcaJ6p-d2Cqg"
+    let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    let AccessScope = "public+read_user+write_likes"
+    let DefaultBaseURL = URL(string: "https://api.unsplash.com/")!
+    
+}
